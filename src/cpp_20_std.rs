8c9a82ba667226029857cb25f7c9889d5f1c2158
Dmitrii - Demenev <demenev.dmitriy1@mail.ru>
2022-01-29 05:16:39 -0700

    Documented C++20 standard implementation

--- src/cpp_20_std.rs
+++ src/cpp_20_std.rs
@@ -13,6 +13,8 @@ use epui::EquisizedPrimitiveUnsignedIntExt as EPUI;
 // }
 // ```
 
+/// Extension trait providing implementation of midpoint algorithm [as suggested for
+/// C++20 standard library](http://www.open-std.org/jtc1/sc22/wg21/docs/papers/2019/p0811r3.html)
 pub trait MidpointViaCpp20StdImplementationExt {
     #[must_use]
     fn midpoint_via_cpp_20_std_implementation(&self /*a_ref*/, b_ref: &Self) -> Self;
@@ -20,6 +22,18 @@ pub trait MidpointViaCpp20StdImplementationExt {
 
 macro_rules! impl_midpoint_fn_for_t {
     () => {
+        /// Returns midpoint using algorithm
+        /// [as suggested for  C++20 standard library](http://www.open-std.org/jtc1/sc22/wg21/docs/papers/2019/p0811r3.html)
+        /// . For primitive integers, the result is rounded towards left argument.
+        ///
+        /// # Examples
+        ///
+        /// ```
+        /// use midpoint::MidpointViaCpp20StdImplementationExt;
+        /// 
+        /// let result: i32 = (-3).midpoint_via_cpp_20_std_implementation(&-2);
+        /// assert_eq!(result, -3);
+        /// ```
         fn midpoint_via_cpp_20_std_implementation(&self /*a_ref*/, b_ref: &Self) -> Self {
             // The line below is impossible due to error[E0401]:
             //  "can't use generic parameters from outer function"
