e362ac0b73df3a55fde51fea19735d17ed8939aa
Dmitrii - Demenev <demenev.dmitriy1@mail.ru>
2022-01-30 03:39:28 -0700

    Better documented benchmarks and provided pre-generated criterion reports

--- benches/my_benchmark.rs
+++ benches/my_benchmark.rs
@@ -1,3 +1,9 @@
+// Benchmarking functions with branching is difficult due to unpredictable performance
+// caused by branch prediction and its high penalties for misprediction.
+// Learn more about impacts of branching from
+// "Branchless Programming in C++ - Fedor Pikus - CppCon 2021"
+// Source: https://youtu.be/g-WPhYREFjk
+
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use midpoint::{
     MidpointViaBitwiseOpsExt, MidpointViaCpp20StdImplementationExt,
@@ -5,6 +11,9 @@ use midpoint::{
 };
 
 macro_rules! bench_fn {
+    // Black box prevents the compiler from optimizing out the loop
+    // for measuring the performance of operations. However,
+    // it can't interfere with branch prediction.
     (
         $group:ident,
         $t:ty,
@@ -74,26 +83,26 @@ macro_rules! bench_all_fns {
     };
 }
 
-macro_rules! benchmark_all_fns_for_t {
+macro_rules! benchmark_all_fns_for_t_on_same_input {
     ($benchmark_name:ident, $t:ident, $a:expr, $b:expr) => {
         fn $benchmark_name(c: &mut Criterion) {
-            let mut group = c.benchmark_group(stringify!($t));
+            let mut group = c.benchmark_group(concat!(stringify!($t), " ", "same input"));
             bench_all_fns!(group, $t, 0, 20);
             group.finish();
         }
     };
 }
 
-benchmark_all_fns_for_t!(benchmark_for_u8, u8, 0, 20);
-benchmark_all_fns_for_t!(benchmark_for_u16, u16, 0, 20);
-benchmark_all_fns_for_t!(benchmark_for_u32, u32, 0, 20);
-benchmark_all_fns_for_t!(benchmark_for_u64, u64, 0, 20);
-benchmark_all_fns_for_t!(benchmark_for_u128, u128, 0, 20);
-benchmark_all_fns_for_t!(benchmark_for_i8, i8, 0, 20);
-benchmark_all_fns_for_t!(benchmark_for_i16, i16, 0, 20);
-benchmark_all_fns_for_t!(benchmark_for_i32, i32, 0, 20);
-benchmark_all_fns_for_t!(benchmark_for_i64, i64, 0, 20);
-benchmark_all_fns_for_t!(benchmark_for_i128, i128, 0, 20);
+benchmark_all_fns_for_t_on_same_input!(benchmark_for_u8, u8, 0, 20);
+benchmark_all_fns_for_t_on_same_input!(benchmark_for_u16, u16, 0, 20);
+benchmark_all_fns_for_t_on_same_input!(benchmark_for_u32, u32, 0, 20);
+benchmark_all_fns_for_t_on_same_input!(benchmark_for_u64, u64, 0, 20);
+benchmark_all_fns_for_t_on_same_input!(benchmark_for_u128, u128, 0, 20);
+benchmark_all_fns_for_t_on_same_input!(benchmark_for_i8, i8, 0, 20);
+benchmark_all_fns_for_t_on_same_input!(benchmark_for_i16, i16, 0, 20);
+benchmark_all_fns_for_t_on_same_input!(benchmark_for_i32, i32, 0, 20);
+benchmark_all_fns_for_t_on_same_input!(benchmark_for_i64, i64, 0, 20);
+benchmark_all_fns_for_t_on_same_input!(benchmark_for_i128, i128, 0, 20);
 
 criterion_group!(
     benches,
